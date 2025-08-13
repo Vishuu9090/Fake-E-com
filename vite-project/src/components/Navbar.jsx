c44388ca2fae6e97f1afa3b03ae3b7e22d67ecfd
Vishuu9090 <vishuu9090@gmail.com>
2025-08-13 15:33:27 +0530

    search optimised

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -6,7 +6,6 @@ export default function Navbar({ search, setSearch }) {
     <nav className="bg-gray-900 text-white px-6 py-3 flex items-center justify-between flex-wrap gap-3">
       {/* Logo */}
       <h2 className="text-2xl font-bold">Fake E-Com</h2>
-
       {/* Search */}
       <div className="relative">
         <input
@@ -14,11 +13,10 @@ export default function Navbar({ search, setSearch }) {
           placeholder="Search..."
           value={search}
           onChange={(e) => setSearch(e.target.value)}
-          className="px-3 py-1 rounded-md text-black bg-white pr-10 sm:w-48  w-150"
+          className="px-3 py-1 rounded-md text-black bg-white pr-10 w-[140px] sm:w-[450px]"
         />
-        <FaSearch className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 " />
+        <FaSearch className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500" />
       </div>
-
       {/* Buttons */}
       <div className="flex gap-3">
         <button className="bg-white text-black px-3 py-1 rounded hover:bg-gray-200">
